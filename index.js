ad1b3b2ab267ffc1a3e72df09c9c4fdbefeed773
Michelle-Mungai <chellymungai@gmail.com>
2022-12-11 11:36:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(let i = 0; i < array.length; i++){
+  const myArray = target - array[i];
+  for (let j = i + 1; j < array.length; j++){
+    if (array[j] === myArray)
+      return true;
+    
+    }
+  }
+  return false;
 }
 
 /* 
